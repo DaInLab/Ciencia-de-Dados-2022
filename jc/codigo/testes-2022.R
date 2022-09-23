9cffba032056ba1c16c7c8df880410256246263a
Joao Pedro Albino <jp.albino@unesp.br>
2022-09-23 17:27:42 -0300

    Inclusão de dois novos projetos - jc e mapa, em: 2022-09-23.

---
+++ jc/codigo/testes-2022.R
@@ -0,0 +1,28 @@
+library(pdftools)
+download.file("http://arxiv.org/pdf/1403.2805.pdf", "1403.2805.pdf", mode = "wb")
+txt <- pdf_text("./dados/1403.2805.pdf")
+
+# first page text
+cat(txt[1])
+
+# second page text
+cat(txt[2])
+
+# Table of contents
+toc <- pdf_toc("./dados/1403.2805.pdf")
+
+# Show as JSON
+jsonlite::toJSON(toc, auto_unbox = TRUE, pretty = TRUE)
+
+# Author, version, etc
+info <- pdf_info("./dados/1403.2805.pdf")
+
+# Table with fonts
+fonts <- pdf_fonts("./dados/1403.2805.pdf")
+
+# renders pdf to bitmap array
+bitmap <- pdf_render_page("./dados/1403.2805.pdf", page = 1)
+
+# save bitmap image
+png::writePNG(bitmap, "page.png")
+webp::write_webp(bitmap, "page.webp")
