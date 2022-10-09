1cd2e5577c4428cfea913ef0255775fd6031bfb5
Joao Pedro Albino <jp.albino@unesp.br>
2022-10-09 11:36:29 -0300

    Atualizando via W-10: 2022-10-09.

---
+++ instalando.R
@@ -0,0 +1,3 @@
+# Intalando o pacote reticulate (<https://rstudio.github.io/reticulate/>)
+# Instruções gerais: <https://support.rstudio.com/hc/en-us/articles/1500007929061-Using-Python-with-the-RStudio-IDE>
+install.packages("reticulate")
