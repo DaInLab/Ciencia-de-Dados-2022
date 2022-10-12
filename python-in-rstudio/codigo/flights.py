0f9d5cfed44000218eb99f662b3a1328d6f5adb1
Your Namejpalbino <jp.albino@unesp.br>
2022-10-12 16:37:10 -0300

    Códigos em Python no Rstudio: 2022-10-12.

--- python-in-rstudio/codigo/flights.py
+++ python-in-rstudio/codigo/flights.py
@@ -1,5 +1,6 @@
 import pandas as pd
-flights = pd.read_csv("D:/Github/Ciencia-de-Dados-2022/python-in-rstudio/dados/flights.csv")
+#flights = pd.read_csv("D:/Github/Ciencia-de-Dados-2022/python-in-rstudio/dados/flights.csv")
+flights = pd.read_csv('~/Library/Mobile Documents/com~apple~CloudDocs/GitHub/Ciencia-de-Dados-2022/python-in-rstudio/dados/flights.csv')
 flights
 
 flights.describe(include='all')
